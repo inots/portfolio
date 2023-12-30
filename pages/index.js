3458d7721d79697d8abe099168f005395cf5962f
inots <45614037+inots@users.noreply.github.com>
2023-12-30 19:32:52 +0200

    merge to main (#1)
    * feat: update content
    
    * feat: update import
    
    * upload resume file
    
    * update location of resume file
    
    * Delete lib/Ino-resume-compressed.pdf
    
    * feat: update fonts
    
    * feat: update text and images
    
    * Add files via upload
    
    * feat: update cycladic image path
    
    * feat: update path of other assets
    
    * feat: update path of portal asset
    
    * Create CNAME
    
    * Rename Cycladic.png to cycladic.png
    
    * Delete CNAME
    
    * Delete public/Ino-resume-compressed.pdf
    
    * Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,21 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Tabs from '@/components/tabs'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>inomade</title>
+        <title>❋ Ino Tsichrintzi ❋</title>
         <meta name="description" content="Portfolio - Ino Tsichrintzi" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div class="heading">
-            <h1 class="title">inomade</h1>
+      <main className={styles.main}>
+        <div className={styles.heading}>
+            <h1 className={styles.title}>Hello ❋ I am Ino, <br/>a software developer</h1>
         </div>
         <Tabs/>
-        {/* <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div> */}
       </main>
     </>
   )
