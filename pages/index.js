494725652037307fa415166d9879103b8e2222a3
Ino Tsichrintzi <ino.tsich@gmail.com>
2023-12-30 15:22:48 +0200

    feat: update fonts

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Tabs from '@/components/tabs'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>inomade</title>
+        <title>Ino Tsichrintzi :)</title>
         <meta name="description" content="Portfolio - Ino Tsichrintzi" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={styles.main}>
         <div className={styles.heading}>
             <h1 className={styles.title}>Hello! I am Ino, <br/>a software developer</h1>
         </div>
