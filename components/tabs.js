8ecf288af8651fb575a935ed7f15f1240f85da51
inots <45614037+inots@users.noreply.github.com>
2024-02-03 12:22:42 -0800

    Mel's design update (#3)
    * feat: update content
    
    * feat: update import
    
    * upload resume file
    
    * update location of resume file
    
    * Delete lib/Ino-resume-compressed.pdf
    
    * feat: update fonts
    
    * feat: update text and images
    
    * Add files via upload
    
    * feat: update cycladic image path
    
    * feat: update path of other assets
    
    * feat: update path of portal asset
    
    * Create CNAME
    
    * Rename Cycladic.png to cycladic.png
    
    * Delete CNAME
    
    * Delete public/Ino-resume-compressed.pdf
    
    * Add files via upload
    
    * feat: pass one
    
    * feat: update styles and insta asset

--- components/tabs.js
+++ components/tabs.js
@@ -4,16 +4,12 @@ import { Tab } from '@headlessui/react';
 import styles from '@/styles/Home.module.css';
 import { getAllContent } from "@/lib/content";
 import ProjectCard from "./projectCard";
-import CarbonDisclaimer from "./carbonDisclaimer";
 
 
-const Tabs = ({updateCurrTab}) => {
+const Tabs = () => {
     const content = getAllContent();
     return (
-        <Tab.Group
-            onChange={(index) => {
-                updateCurrTab(index);
-            }}>
+        <Tab.Group>
             <Tab.List className={styles.navigation}>
                 {content.map((tab) => (
                     <Tab className={({ selected }) => selected ? styles.selected : styles.tablink} key={tab.id}>
@@ -24,14 +20,11 @@ const Tabs = ({updateCurrTab}) => {
             <Tab.Panels className={styles.content}>
                 {content.map((p) => (
                     <Tab.Panel className={p.content ? `${styles.tabcontent} ${styles.paragraph}` : styles.tabcontent} key={p.id}>
-                        <div className={p.description == "Hello" ? styles.intro : ""}>
-                            {p.content ? 
-                                p.content.map((c) => (
-                                    <div key={p.content.indexOf(c)}>{c}</div>
-                                ))
-                            : <ProjectCard/> }
-                        </div>
-                        <div className={styles.notes}>
+                        {p.content ? 
+                            p.content.map((c) => (
+                                <div key={p.content.indexOf(c)}>{c}</div>
+                            ))
+                        : <ProjectCard/> }
                             {p.links?.map((link) => (
                                 <p key={link.id}>
                                     <Link href={link.url} className={styles.link}>
@@ -39,10 +32,6 @@ const Tabs = ({updateCurrTab}) => {
                                     </Link>
                                 </p>
                             ))}
-                        </div>
-                        {p.title == "About" ? 
-                            <CarbonDisclaimer />
-                        : ""}
                         </Tab.Panel>
                     ))}
             </Tab.Panels>
